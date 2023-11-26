6607090be88597de710841aa7532f126194bc6f6
Mandy <Lmandy239@gmail.com>
2023-11-26 12:18:39 -0500

    Finished product

--- README.md
+++ README.md
@@ -1,10 +1,29 @@
 # FoodTruckProject
-
+Hey welcome to the food truck storing device! 
 
 #Description
+This app helps users input the name, food type and rating of their preferred food trucks. Then it will store the information and display at users discretion. You may also choose to display the average ratings, or the highest-rated truck.
+
+Instructions:
+BEFORE YOU START: some invalid answers will cause the program to terminate.
+
+1- Start the program.
+2- Follow the initial prompt
+3- Type in the name, food type and rating of a food truck
+4- YOu may choose to leave during the name input prompt by simply typing "Quit"
+5- App will run the list program until you choose to not add more trucks or finish listing all
+
+Notes: 
+-Food type refers to a type of food, not a specific dish (for example: mexican, italian, cuban etc). Rating average will calculate higher than 5, but then the standard will be up to the user. 
+
+-Another bug is the names not being able to type with spaces for example:
+CubanTruck will print out but Cuban Truck will print as Cuban. This is most likely an issue within my toString method in the other file. 
+
+-Could have built multple toStrings to have different outputs depending on the situation. For simplicity I chose to stick with the same output for all scenarios.
 
 
 #Tech Used
-
+All of this was written in java, most of the work was done using separate classes and methods. There is usage of a few for loops and a switch statement for the main menu. 
 
 #Lessons Learned
+I learned a lot about methods and passing down fields, I had a bit of trouble with the logic, but mostly due to my own confusion. Moreover, I kept adding a lot to the program without testing little by little, before I knew it I had a mess of stuff that can cause some bugs or things to not work according to the plan.  I think next time I will keep the code dry and try to only make things work little by little.
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -7,6 +7,7 @@ import com.skilldistillery.foodtruck.entities.FoodTruck;
 public class FoodTruckApp {
 
 	private FoodTruck[] fleet = new FoodTruck[5];
+	private int truckCount = 0;
 
 	public static void main(String[] args) {
 		FoodTruckApp app = new FoodTruckApp();
@@ -18,43 +19,208 @@ public class FoodTruckApp {
 		int counter = 0;
 		String answer = "";
 		do {
-			System.out.println("Welcome, please enter the name, food type, and rating for " + "up to five food trucks");
-			System.out.println("You may also choose to quit at anytime by typing: (Quit) when prompted to.");
-			
-		
-                System.out.print("Name of the truck (or type 'Quit' to exit): ");
-                 String truckName = input.next();
+			System.out.println("Welcome, please enter the name, food type, and rating for up to five food trucks");
+			System.out.println("You may also choose to quit at any time by typing: (Quit) when prompted to.");
 
-                if (truckName.equalsIgnoreCase("Quit")) {
-                    break;
-                }
+			System.out.print("Name of the truck (or type 'Quit' to exit): ");
+			String truckName = input.next();
 
-                System.out.print("What's the Food type?: ");
-                String foodType = input.next();
-                
-				System.out.println("How would you rate it?(out of 5 stars): ");
+			if (truckName.equalsIgnoreCase("Quit")) {
+				break;
+			}
 
-				int rating = input.nextInt();
-				
-				FoodTruck foodTruck = new FoodTruck(truckName, foodType, rating);
-				foodTruck.displayFoodTruck();
-				System.out.println(" ");
-				System.out.println("Would you like to add another truck or quit? Yes (Y) or No (N)");
-				answer = input.next();
+			System.out.print("What's the Food type?: ");
+			String foodType = input.next();
+
+			System.out.println("How would you rate it? (out of 5 stars): ");
+			int rating = input.nextInt();
 			
-			
-			counter ++;
-			
-		} while (counter <= 5 && answer.equalsIgnoreCase("Y"));
+			FoodTruck foodTruck = new FoodTruck(truckName, foodType, rating);
+			fleet[truckCount++] = foodTruck;
+
+			System.out.println("Food truck added successfully!");
+			System.out.println();
+			foodTruck.displayFoodTruck();
+			System.out.println();
+			System.out.println("Would you like to add another truck? Yes (Y) or No (N)");
+			answer = input.next();
+
+			if (answer.equalsIgnoreCase("N")) {
+				break;
+			}
+
+			counter++;
+
+		} while (counter < 6 && answer.equalsIgnoreCase("Y"));
+
+		if (answer.equalsIgnoreCase("N")) {
+			displayMenu();
+			answer = input.next();
+
+			switch (answer.toLowerCase()) {
+			case "1":
+				listAllFoodTrucks();
+				break;
+			case "2":
+				CalculateRating();
+				break;
+			case "3":
+				HighestRated();
+				break;
+			case "4":
+				System.out.println("Goodbye!");
+				input.close();
+				System.exit(0);
+			default:
+				System.out.println("Invalid option. Please choose again.");
+			}
+		}
+	}
+
+	public void displayMenu() {
+		System.out.println("Select an option:");
+		System.out.println("1. List all existing food trucks.");
+		System.out.println("2. See the average rating of food trucks.");
+		System.out.println("3. Display the highest-rated food truck.");
+		System.out.println("4. Quit the program.");
+	}
+
+	public void listAllFoodTrucks() {
+		Scanner input2 = new Scanner(System.in);
+		System.out.println("Listing all food trucks:");
+		for (int i = 0; i < truckCount; i++) {
+			fleet[i].displayFoodTruck();
+		}
+		System.out.println("Would you like to start over? (Y/N)");
+		String answer = input2.next();
+		if (answer.equalsIgnoreCase("Y")) {
+			run();
+		} else if (answer.equals("N")) {
+			System.exit(0);
+		} else {
+			System.out.println("Going back to the menu");
+			if (answer.equalsIgnoreCase("N")) {
+				displayMenu();
+				answer = input2.next();
+
+				switch (answer.toLowerCase()) {
+				case "1":
+					listAllFoodTrucks();
+					break;
+				case "2":
+					CalculateRating();
+					break;
+				case "3":
+					HighestRated();
+					break;
+				case "4":
+					System.out.println("Goodbye!");
+					input2.close();
+					System.exit(0);
+				default:
+					System.out.println("Invalid option. Please choose again.");
+				}
+			}
+		}
+	}
+
+	public void CalculateRating() {
+		Scanner input2 = new Scanner(System.in);
+		if (truckCount == 0) {
+			System.out.println("No food trucks to calculate average rating.");
+			return;
+		}
+
+		int totalRating = 0;
+		for (int i = 0; i < truckCount; i++) {
+			totalRating += fleet[i].getAverageRating();
+		}
+
+		float averageRating = (float) totalRating / truckCount;
+		System.out.println("Average rating of food trucks: " + averageRating);
+		System.out.println("Would you like to start over? (Y/N)");
+		String answer = input2.next();
+		if (answer.equalsIgnoreCase("Y")) {
+			run();
+		} else if (answer.equals("N")) {
+			System.exit(0);
+		} else {
+			System.out.println("Going back to the menu");
+			if (answer.equalsIgnoreCase("N")) {
+				displayMenu();
+				answer = input2.next();
+
+				switch (answer.toLowerCase()) {
+				case "1":
+					listAllFoodTrucks();
+					break;
+				case "2":
+					CalculateRating();
+					break;
+				case "3":
+					HighestRated();
+					break;
+				case "4":
+					System.out.println("Goodbye!");
+					input2.close();
+					System.exit(0);
+				default:
+					System.out.println("Invalid option. Please choose again.");
+
+				}
+			}
+		}
+	}
+
+	public void HighestRated() {
+		Scanner input2 = new Scanner(System.in);
+		if (truckCount == 0) {
+			System.out.println("No food trucks to display.");
+			return;
+		}
+
+		FoodTruck highestRatedTruck = fleet[0];
+		for (int i = 1; i < truckCount; i++) {
+			if (fleet[i].getAverageRating() > highestRatedTruck.getAverageRating()) {
+				highestRatedTruck = fleet[i];
+			}
+		}
+
+		System.out.println("Highest-rated food truck:");
+		highestRatedTruck.displayFoodTruck();
+		System.out.println("\n");
+		System.out.println("Would you like to start over? (Y/N)");
+		String answer = input2.next();
+		if (answer.equalsIgnoreCase("Y")) {
+			run();
+		} else if (answer.equals("N")) {
+			System.out.println("Goodbye!");
+		} else {
+
+			System.out.println("Going back to the menu");
+			if (answer.equalsIgnoreCase("N")) {
+				displayMenu();
+				answer = input2.next();
+
+				switch (answer.toLowerCase()) {
+				case "1":
+					listAllFoodTrucks();
+					break;
+				case "2":
+					CalculateRating();
+					break;
+				case "3":
+					HighestRated();
+					break;
+				case "4":
+					System.out.println("Goodbye!");
+					input2.close();
+					System.exit(0);
+				default:
+					System.out.println("Invalid option. Please choose again.");
+				}
+			}
+		}
 
 	}
-	
-
-    private void displayMenu() {
-        System.out.println("Select an option:");
-        System.out.println("1. List all existing food trucks.");
-        System.out.println("2. See the average rating of food trucks.");
-        System.out.println("3. Display the highest-rated food truck.");
-        System.out.println("4. Quit the program.");
-    }
 }
